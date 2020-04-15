d93fa3234e27025ce898303bf09474626f7404ba
Gunel Khantamirova <gun040el@gmail.com>
2020-04-15 03:09:04 -0400

    first commit

---
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -0,0 +1,25 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * It means to be extend
+ */
+
+public class AbstractBasePage {
+
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+
+    public AbstractBasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+}
